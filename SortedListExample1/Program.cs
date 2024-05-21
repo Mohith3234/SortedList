1f955d8fe6c7c97123f2395b6aac6feb421b1395
Koninti Mohithkumar Goud <al6127@accionlabs.in>
2024-05-21 11:11:26 +0530

    first commit

---
+++ SortedListExample1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SortedListExample1
+{
+     class Program
+    {
+        static void Main()
+        {
+            SortedList<int, string> employees = new SortedList<int, string>()
+            {
+                { 102, "Smith" },
+                { 105, "James" },
+                { 103, "Allen" },
+                { 101, "Scott" },
+                { 104, "Jones" }
+            };
+            //add the element
+            employees.Add(106, "mets");
+            //remove element 
+            employees.Remove(103);
+            //ContainsKey
+            bool k = employees.ContainsKey(102);
+            Console.WriteLine("Contains Key : "+k);
+            bool v = employees.ContainsValue("smith");
+            Console.WriteLine("Contains value : " + v);
+            //index of key
+            int x = employees.IndexOfKey(102);
+            Console.WriteLine("index of 102 key : "+x);
+            int y = employees.IndexOfValue("mets");
+            Console.WriteLine("index of mets value : "+y);
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Key+","+employee.Value);
+
+                
+            }
+            employees.Clear();
+
+            Console.ReadKey();
+        }
+    }
+}
